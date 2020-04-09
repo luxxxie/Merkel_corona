6373e86b77265522f6ba3d9ae1fc0ea0f0cf4d23
luxxxie <hanoul@gmail.com>
2020-04-09 15:20:23 +0200

    more code

--- script.R
+++ script.R
@@ -1,5 +1,14 @@
 install.packages("remotes")
 remotes::install_github("lingeringcode/tweetscoresmod")
 library(readr)
+library(tidyverse)
+library(tweetscoresmod)
 data <- read_csv("data.csv")
 View(data)
+userid_str_de <- data %>% 
+  filter(language == "DE") %>% 
+  distinct() %>% 
+  select(User_id) %>%
+  as.list()
+
+
