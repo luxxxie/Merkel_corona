df9b60d5a38ec2461754d1e31602c73591b37aef
luxxxie <hanoul@gmail.com>
2020-04-10 13:37:50 +0200

    waiting for dev twitter account

--- script.R
+++ script.R
@@ -2,7 +2,9 @@ install.packages("remotes")
 remotes::install_github("lingeringcode/tweetscoresmod")
 library(readr)
 library(tidyverse)
+library(rtweet)
 library(tweetscoresmod)
+load("~/my_oauth")
 data <- read_csv("data.csv")
 View(data)
 userid_str_de <- data %>% 
@@ -11,4 +13,8 @@ userid_str_de <- data %>%
   select(User_id) %>%
   as.list()
 
+## get twitter ideologies according to barbera
+my_oath
 
+followers <- getFriends(user_id = userid_str_de,
+                        oauth = my_oath)
